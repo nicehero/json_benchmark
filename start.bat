d9643dd464373a15e65ec766488b5a59be7fe0d4
nicehero <85948597@qq.com>
2022-01-11 22:19:08 +0800

    first commit

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 g++ -O test.cpp -I./
 a >> result.txt
 pip3 install motor -i https://pypi.douban.com/simple
+pip3 install base58 -i https://pypi.douban.com/simple
 python3 py.py >> result.txt
 PAUSE
